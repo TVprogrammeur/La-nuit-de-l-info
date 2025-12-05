2ba3adf9007b3a339164e80c199b8036af3b5951
Robin F. <robinferreira@yahoo.com>
2025-12-05 04:42:05 +0100

    ajout logo

---
+++ get_site.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+httrack "$1" -O "$2" --depth=1 --mirrorlinks --stay-on-same-domain --ext-depth=0 --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131 Safa^C/537.36"
